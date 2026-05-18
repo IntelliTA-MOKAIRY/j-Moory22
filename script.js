1cc082a91734205bb2a4c997ab77ab4005810584
MESHAL OKAIRY <60521843+MOKAIRY@users.noreply.github.com>
2026-05-19 00:11:33 +0300

    Submit solution via intelliTA

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// Add your JavaScript here
+  let count = 0;
+
+  // Checkpoint 1: make increment() increase count by 1
+  function increment() {
+    // TODO
+    count++;
+  }
+
+  // Checkpoint 2: make decrement() decrease count by 1
+  function decrement() {
+    // TODO
+    count--;
+  }
+
+  // Checkpoint 3: make getCount() return the current count
+  function getCount() {
+    // TODO
+  }
+
+  // Checkpoint 4: make displayCount() show the count in the #count element
+  function displayCount() {
+    // TODO
+  }
+
+  // Wiring (already done for you)
+  document.getElementById("increaseBtn").addEventListener("click", () => { increment(); displayCount(); });
+  document.getElementById("decreaseBtn").addEventListener("click", () => { decrement(); displayCount(); });
+  displayCount();
